2783aa07d345b9db3e827d2cdbb0087e75a15456
Paul <123371314+Xenmas97@users.noreply.github.com>
2023-08-10 11:26:23 +0000

    Add files via upload

--- app_4/pages/Layout.py
+++ app_4/pages/Layout.py
@@ -7,7 +7,7 @@ st.title("Layout avec Streamlit")
 
 """
 Streamlit permet de gérer la disposition des widgets de manière très simple avec la fonction ```st.columns```. Il permet
-d'organiser les widgets (ou du texte selo, ce que l'on souhaite afficher) en colonne en divisant l'interface. Le nombre
+d'organiser les widgets (ou du texte selon ce que l'on souhaite afficher) en colonne en divisant l'interface. Le nombre
 de colonne est passé en argument à la fonction. Par exemple:
 """
 
@@ -15,18 +15,35 @@ col1, col2 = st.columns(2)
 
 with col1:
     """
-    Nous pouvons diviser la zone en 2 colonnes
+    ## What is Lorem Ipsum?
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+    standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a 
+    type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+    Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of 
+    Lorem Ipsum.
     """
 
 with col2:
     """
-    Et disposer nos élément sur l'interface
+    ## Why do we use it?
+    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its
+     layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to 
+     using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web 
+     page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web 
+     sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on 
+     purpose (injected humour and the like).
     """
 
 """
-C'est un bon moyens de concevoir des tableaux de bord en disposant plusieurs graphiques sur une même ligne. Imaginons
-que l'on souhaite afficher 2 graphiques sur la même ligne:
+C'est un bon moyens de concevoir des tableaux de bord en disposant plusieurs graphiques sur une même ligne. Cependant,
+si l'on dispose des graphiques ainsi et  que l'on modifie la taille de la page, les graphuques ne vont pas s'adapter à 
+cette dernière. Si nous souhaitons avoir des éléments qui adapte automatiquement sa largeur à la page, il faut inclure 
+ces éléments dans un **conteneur**. Cela peut se faire avec la commande ```st.container```. Il faut également inclure 
+dans la fonction de sortie l'argument ```use_container_width```. Imaginons que l'on souhaite afficher 2 graphiques sur 
+la même ligne:
 """
+
 
 @st.cache_data
 def database():
@@ -34,20 +51,29 @@ def database():
     return pd.read_csv("app_4/Cancer_Data.csv").drop(["id", "Unnamed: 32"], axis=1)
 
 
-col3, col4 = st.columns(2)
+with st.container():
+    col3, col4 = st.columns(2)
+    with col3:
+        diagnosis_count = (
+            database().groupby("diagnosis").size().reset_index(name="Counts")
+        )
+        diagnosis_distribution = px.bar(
+            data_frame=diagnosis_count,
+            x="diagnosis",
+            y="Counts",
+            color="diagnosis",
+            title="Distribution de la variable diagnosis",
+        )
+        st.plotly_chart(diagnosis_distribution, use_container_width=True)
 
-with col3:
-    diagnosis_count = database().groupby("diagnosis").size().reset_index(name="Counts")
-    diagnosis_distribution = px.bar(
-        data_frame=diagnosis_count,
-        x="diagnosis",
-        y="Counts",
-        color="diagnosis",
-        title="Distribution de la variable diagnosis"
-    )
-    st.plotly_chart(diagnosis_distribution)
+    with col4:
+        show_column = st.selectbox(
+            label="Colonne", options=database().drop(["diagnosis"], axis=1).columns
+        )
+        variable_distribution = px.histogram(database(), x=f"{show_column}")
+        st.plotly_chart(variable_distribution, use_container_width=True)
 
-with col4:
-    show_column = st.selectbox(label="Colonne", options=database().drop(["diagnosis"], axis=1).columns)
-    variable_distribution = px.histogram(database(), x=f"{show_column}")
-    st.plotly_chart(variable_distribution)
+"""
+En modifiant les dimensions de la page, les 2 graphiques vont ajuster leurs dimensions et leurs positions 
+automatiquement.
+"""
--- app_4/widget.py
+++ app_4/widget.py
@@ -9,9 +9,11 @@ st.title("Familiarisation avec les Widgets de streamlit")
 # Slider (barre de valeur)
 """
 
+st.divider()
 """
 Ce widget crée une barre horizontale de valeur. Par exemple:
 """
+st.divider()
 
 # Barre de défilement
 x = st.slider(label="x")
@@ -26,9 +28,12 @@ st.code(f"La valeur de x est {x} \n" f"le carré de x est {x**2}")
 # Text input (zone de saisie de texte)
 """
 
+st.divider()
 """
 Ce widget permet à l'utilisateur de saisir du texte. Par exemple:
 """
+st.divider()
+
 # zone de saisie
 nom = st.text_input(label="Ton nom")
 
@@ -37,12 +42,15 @@ st.markdown(" De même, il est possible d'afficher la saisie de l'utilisateur:")
 st.code(f"Ton nom est {nom}")
 
 """
-# chechbox
+# checkbox
 """
+
+st.divider()
 """
 Ce widget permet entre autre d'afficher ou de masquer une section de son application. Par exemple, on peut decider
 d'afficher ou non une base de données:
 """
+st.divider()
 
 
 @st.cache_data
@@ -55,7 +63,8 @@ data = database()
 # Affichage de la base de données au choix de l’utilisateur
 if st.checkbox(label="Show database"):
     data = database()
-    st.dataframe(data.style.highlight_max(axis=0))
+    st.dataframe(data, hide_index=True)
+
 
 """
 En cochant la case, l'utilisateur peut faire apparaitre la base de données
@@ -66,10 +75,14 @@ En cochant la case, l'utilisateur peut faire apparaitre la base de données
 # Selectbox
 """
 
+st.divider()
 """
 Ce Widget lui est utilisé lorsqu'on veut faire un choix parmis plusieurs valeurs. Par exemple, on peut l'utiliser pour 
-selectionner la colonne de notre base de données que l'on souhaite afficher
+sélectionner la colonne de notre base de données que l'on souhaite afficher
 """
+st.divider()
+
+
 show_colomn = st.selectbox(
     label="Colonne",
     options=database().columns
@@ -80,11 +93,34 @@ st.dataframe(database().loc[:, [show_colomn]])
 L'utilisateur choisis ainsi la colonne qu'il souhaite visualiser
 """
 
-
+st.divider()
 """
 **Note**: Il est possible de créer un widget sans avoir à le mettre dans une variable. Auquel cas, afin d'afficher sa
- valeur, il est necessaire de lui associer une clé afin de pouvoir identifier de manière unique chaque widget (car il 
- est possible d'avoir plusieurs ```Slider``` ou ```Text input``` sur l'interface. Cela ce fait avec l'argument ```key```
- qui prend en valeur une chaîne de caractère. Lorsque cela est fait, la valeur du widget est accesible via la commande
-  ```st.session_state.key``` où ```key``` est la clé du widget
+valeur, il est necessaire de lui associer une clé afin de pouvoir identifier de manière unique chaque widget (car il 
+est possible d'avoir plusieurs ```Slider``` ou ```Text input``` sur l'interface. Cela ce fait avec l'argument ```key```
+qui prend en valeur une chaîne de caractère. Lorsque cela est fait, la valeur du widget est accesible via la commande
+```st.session_state.key``` où ```key``` est la clé du widget. ```st.session_state``` est un **dictionnaire** receuillant
+l'ensemble des valeurs des widgets dont on a initialisé la clé. Ici on peut acceder aux valeurs de la session en 
+l'appelant:
 """
+st.divider()
+
+st.write(st.session_state)
+
+st.divider()
+"""
+Comme on peut le voir, elle ne contient pas les valeurs des widgets que l'on a crée car tout ils tous ont été stocké 
+dans des variables. Maintenant créeons un widget dont on va initialisé la clé. Cette dernière va s'afficher dans la
+session (le nom de la clé et sa valeur)
+"""
+st.divider()
+st.slider(label="valeur", key="slider")
+st.divider()
+
+"""
+L'utilisation de la session est un bon moyen de stocker certains resultat entre plusieurs exécutions de l'application
+(Streamlit réexecute entierement le script python à chaque fois que l'utilisateur va interagir avec un widget de 
+l'application). **Les valeurs des widgets dans la session peuvent être utilisé dans toutes les pages d'une application 
+streamlit.**
+"""
+st.divider()
